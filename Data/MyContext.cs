496c55ffa25f09b60e57b959b366e428d1955546
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-09-13 15:40:25 +0330

    Add project files.

---
+++ Data/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreSample.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreSample.Data
+{
+    internal class MyContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=EFCoreSample;User ID=sa;Password=123;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using EFCoreSample.Data;
+using EFCoreSample.Models;
+
+namespace EFCoreSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using MyContext context = new MyContext();
+
+            #region Add
+
+            //Product veggieSpecial = new Product()
+            //{
+            //    Name = "Veggie Special Pizza",
+            //    Price = 9.99M
+            //};
+            //context.Products.Add(veggieSpecial);
+
+            //Product deluxeMeat = new Product()
+            //{
+            //    Name = "Deluxe Meat Pizza",
+            //    Price = 12.99M
+            //};
+            //context.Add(deluxeMeat);
+
+            //context.SaveChanges();
+            #endregion Add
+
+            #region Update
+            //var veggieSpecial = context.Products
+            //    .Where(p => p.Name == "Veggie Special Pizza")
+            //    .FirstOrDefault();
+
+            //if (veggieSpecial is Product)
+            //{
+            //    veggieSpecial.Price = 10.99M;
+            //}
+
+            //context.SaveChanges();
+            #endregion Update
+
+            #region Update
+            var veggieSpecial = context.Products
+                .Where(p => p.Name == "Veggie Special Pizza")
+                .FirstOrDefault();
+
+            if (veggieSpecial is Product)
+            {
+               context.Remove(veggieSpecial);
+            }
+
+            context.SaveChanges();
+            #endregion Update
+
+            #region Display
+
+            //var products = context.Products
+            //    .Where(p => p.Price > 10.00M)
+            //    .OrderBy(p => p.Name);
+
+
+            //foreach (Product p in products)
+            //{
+            //    Console.WriteLine($"Id:    {p.Id}");
+            //    Console.WriteLine($"Name:  {p.Name}");
+            //    Console.WriteLine($"Price: {p.Price}");
+            //    Console.WriteLine(new string('-', 20));
+            //}
+
+            #endregion Display
+
+        }
+    }
+}
